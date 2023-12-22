4fa6c4d1fde69994bec490c27ddfbda68a29c208
AADEESH BALI <officialaadeesh76@gmail.com>
2023-12-22 12:23:19 +0530

    changes made

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: ["/task4/"],
   plugins: [react()],
-  rollupOptions: {
-    external: ["react", "react-router", "react-router-dom", "react-redux"],
-    output: {
-      globals: {
-        react: "React",
-      },
-    },
-  },
 });
