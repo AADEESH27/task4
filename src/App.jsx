0099d410371f6e869a208384145aaa6620163b6b
AADEESH BALI <officialaadeesh76@gmail.com>
2023-12-22 12:17:08 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Login from "./components/Login";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/task4/" element={<Login />} />
+        <Route path="/home" element={<Dashboard />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Home from "./Home";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Header />
+      <Home />
+    </div>
+  );
+};
+
+export default Dashboard;
